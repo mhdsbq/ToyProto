eb1210a466eb37237a2d9a490cc229eba60d329a
mhdsbq <mhdsbk2@gmail.com>
2026-09-08 23:19:44 +0530

    refactor(compiler): update parser Expect method to improve readability

--- src/ToyProto.Compiler/Parse/Parser.cs
+++ src/ToyProto.Compiler/Parse/Parser.cs
@@ -42,7 +42,7 @@ internal class Parser : IParser
             return null;
 
         Expect(TokenType.Equals, "Expected '=' after 'syntax' keyword");
-        var version = Expect(TokenType.StringLiteral, "Expected string literal after '=' in 'syntax' declaration");
+        Expect(TokenType.StringLiteral, out var version, "Expected string literal after '=' in 'syntax' declaration");
         Expect(TokenType.Semicolon);
 
         return version.Value;
@@ -50,7 +50,7 @@ internal class Parser : IParser
 
     private MessageDeclaration ParseMessage()
     {
-        var name = Expect(TokenType.Identifier, "Expected identifier after 'message' keyword");
+        Expect(TokenType.Identifier, out var name, "Expected identifier after 'message' keyword");
         Expect(TokenType.LeftBrace);
 
         var fields = new List<FieldDeclaration>();
@@ -64,12 +64,11 @@ internal class Parser : IParser
 
     private FieldDeclaration ParseField()
     {
-        var type = Expect(TokenType.Identifier);
-        var name = Expect(TokenType.Identifier);
-
+        Expect(TokenType.Identifier, out var type, "Expected field type");
+        Expect(TokenType.Identifier, out var name, "Expected field name");
         Expect(TokenType.Equals);
+        Expect(TokenType.IntegerLiteral, out var number, "Expected field number");
 
-        var number = Expect(TokenType.IntegerLiteral);
         if (!int.TryParse(number.Value, out var fieldNumber))
             throw new UnexpectedTokenException(TokenType.IntegerLiteral, number, "Field number must be a valid integer");
 
@@ -78,9 +77,28 @@ internal class Parser : IParser
         return new FieldDeclaration(type.Value!, name.Value!, fieldNumber);
     }
 
-    private Token Expect(TokenType tokenType, string? message = null)
+    private void Expect(TokenType tokenType, string? message = null)
     {
         if (_cursor.Match(tokenType, out var token))
+            return;
+
+        throw new UnexpectedTokenException(tokenType, token, message);
+    }
+
+    // Just for ease of read :)
+    //
+    // Earlier:
+    //      Expect(TokenType.Equals, "Expected equal");
+    //      var version = Expect(TokenType.StringLiteral, "Expected string literal");
+    //      Expect(TokenType.Semicolon);
+    //
+    // Now:
+    //      Expect(TokenType.Equals, out var equalsToken, "Expected equal");
+    //      Expect(TokenType.StringLiteral, out var versionToken, "Expected string literal");
+    //      Expect(TokenType.Semicolon, out var semicolonToken);
+    private Token Expect(TokenType tokenType, out Token token, string? message = null)
+    {
+        if (_cursor.Match(tokenType, out token))
             return token;
 
         throw new UnexpectedTokenException(tokenType, token, message);
